461b0caceca9a2fc5f08f0ccde942e0268ec2ad1
tomohiro <tomohiro@fenrir.co.jp>
2020-07-31 01:37:30 +0900

    Update:デザインの修正

--- BasicFrontendKnowledge/script.js
+++ BasicFrontendKnowledge/script.js
@@ -1,13 +1,12 @@
 function onButtonClick() {
     target = document.getElementById("output");
     var num = target.innerText = document.forms.form_id.text_box_id.value;
-    console.log(num);
     if(num == 16){
-        var doc0= document.getElementById("div0");  
-        doc0.innerHTML= "正解！"; 
+        var doc0= document.getElementById("judgment");  
+        doc0.innerHTML= "Correct!"; 
     } else {
-        var doc0= document.getElementById("div0");  
-        doc0.innerHTML= "不正解！"; 
+        var doc0= document.getElementById("judgment");  
+        doc0.innerHTML= "Wrong！"; 
     }
     target.innerText = num;
 }
